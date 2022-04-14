610fe996a7da4a1d0f542291fc909c7c3dd7cad5
wagner <wagncarv@outlook.com>
2022-04-14 15:09:48 -0300

    interpolcao string, formatar moeda

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyHomePage extends StatelessWidget {
                             border: Border.all(color: Colors.purple, width: 2)),
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 20,
