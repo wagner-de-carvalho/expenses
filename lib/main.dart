2e1f5c4ea324044fb210f6afbd27bb5c0a15e08d
wagner <wagncarv@outlook.com>
2022-04-14 14:52:07 -0300

    aplicando estilo titulo e data

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,14 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Column(
-                        children: [Text(tr.title), Text(tr.date.toString())],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tr.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(tr.date.toString(),
+                              style: TextStyle(color: Colors.grey[600]))
+                        ],
                       )
                     ],
                   ),
